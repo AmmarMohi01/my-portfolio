a186adafb61f3d093c686214f9159ba46dcd6bcd
AmmarMohi01 <ammarmohi99@gmail.com>
2025-08-04 01:26:04 +0530

    completed portfolio

---
+++ src/test/java/in/AmmarMohiuddin/Portfolio/PortfolioApplicationTests.java
@@ -0,0 +1,12 @@
+package in.AmmarMohiuddin.Portfolio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = PortfolioApplication.class)
+class PortfolioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
